c38e96bc500ee3da4d9c86165ae6f98ebf031422
Márton Visnovitz <vimtaai@inf.elte.hu>
2020-12-28 10:23:34 +0100

    Multiple destinations, no prerequisites input

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,19 @@
 import { calculateTravelOrder } from "./index.js";
 
+function checkDestinations(travelOrder, destinations) {
+  return destinations.every((destination) => travelOrder.includes(destination));
+}
+
 test("Single destination", () => {
   const input = `x =>`;
 
   expect(calculateTravelOrder(input)).toBe("x");
+});
+
+test("Multiple destinations, no prerequisites", () => {
+  const input = `x =>\ny =>\nz =>`;
+  const travelOrder = calculateTravelOrder(input);
+  const allDestinationsExist = checkDestinations(travelOrder, ["x", "y", "z"]);
+
+  expect(allDestinationsExist).toBe(true);
 });
